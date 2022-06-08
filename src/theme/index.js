5084c621b65e5a511a136f332dd3b388868d121e
(❁´◡`❁) <17709252+coolscoder@users.noreply.github.com>
2022-06-09 00:41:27 +0900

    add theme colors

--- src/theme/index.js
+++ src/theme/index.js
@@ -10,6 +10,32 @@ const theme = createTheme({
     secondary: {
       main: '#19857b',
     },
+    green: {
+      main: '#4caf50',
+      light: '#8ceeb5',
+      dark: '#27ae60',
+    },
+    grey: {
+      main: '#9e9e9e',
+      light: '#f1f4f8',
+      dark: '#a9abae',
+      darker: '#323234',
+    },
+    light: {
+      main: '#ffffff'
+    },
+    red: {
+      main: '#ff5555',
+      light: '#ffa6a6',
+    },
+    yellow: {
+      main: '#ffe68e',
+      light: '#fff9bf',
+      dark: '#ffc700',
+    },
+    border: {
+      main: '#dadcdd',
+    },
     error: {
       main: red.A400,
     },
